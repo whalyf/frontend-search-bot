0221a335efb08e61f09bcee46506acd5cb2474da
whalyf viana <whalyfeng@gmail.com>
2024-04-27 17:25:30 -0300

    remove some fields(location, frequency) not used

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,56 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
-  const [searchParams, setSearchParams] = useState({
-    location: 'Localização',
-    frequency: 'daily',
+  const [searchParams, setSearchParams] = useState<{email: string, keywords:string}>({
     email: '',
     keywords: 'Palavras chave',
   });
 
   
-
-
   const handleProcess = async () => {
-    console.log(searchParams)
     try {
       const response = await axios.post('http://localhost:8080/process', {...searchParams, searchId: uuidv4()});
-      console.log(response);
-      // alert(response.data.message)
+      if(response.status === 200) {
+        toast.success('Obrigado, o diagnóstico será processado e enviado por email')
+      }
     } catch (error) {
-      console.error('Error during search:', error);
+      console.error('Error at search:', error);
     }
   };
   
 
   return (
-    <main>
-      <h1>Google Search Digger</h1>
+    <>
+      <ToastContainer />
+      <main>
+        <h1>Google Search Digger</h1>
 
-      <div className='keywords-section'>
-        <label>Keywords:</label>
-        <input
-          type="text"
-          placeholder='Palavras chave'
-          onChange={(e) => setSearchParams({ ...searchParams, keywords: e.target.value })}
-        />
-      </div>
-      
-      <div className='location-section'>
-        <label>Location:</label>
-        <input
-          type="text"
-          placeholder="Localização"
-          onChange={(e) => setSearchParams({ ...searchParams, location: e.target.value })}
-        />
-      </div>
+        <div className='keywords-section'>
+          <label>Keywords:</label>
+          <input
+            type="text"
+            placeholder='Palavras chave'
+            onChange={(e) => setSearchParams({ ...searchParams, keywords: e.target.value })}
+          />
+        </div>
 
-      {/* <div className='frequency'>
-        <label>Frequency:</label>
-        <select
-          value={searchParams.frequency}
-          onChange={(e) => setSearchParams({ ...searchParams, frequency: e.target.value })}
-        >
-          <option value="daily">Daily</option>
-          <option value="weekly">Weekly</option>
-        </select>
-      </div> */}
-
-      <div className='email-section'>
-        <label>Email Corporativo:</label>
-        <input
-          type="text"
-          placeholder='Email'
-          onChange={(e) => setSearchParams({ ...searchParams, email: e.target.value })}
-        />
-      </div>
-      
-      <button onClick={handleProcess}>Descobrir</button>
-    </main>
+        <div className='email-section'>
+          <label>Email Corporativo:</label>
+          <input
+            type="text"
+            placeholder='Email'
+            onChange={(e) => setSearchParams({ ...searchParams, email: e.target.value })}
+          />
+        </div>
+        
+        <button onClick={handleProcess}>Descobrir</button>
+      </main>
+    </>
+   
   );
 }
 
